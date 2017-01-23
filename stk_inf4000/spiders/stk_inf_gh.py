12077f32469dc0d7b86fd77f965738c6284a4471
Dirk Hesse <herr.dirk.hesse@gmail.com>
2017-01-23 18:12:05 +0100

    Initial commit

---
+++ stk_inf4000/spiders/stk_inf_gh.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from bs4 import BeautifulSoup
+from urlparse import urljoin
+
+class StkInfGhSpider(scrapy.Spider):
+    name = "stk_inf_gh"
+    start_urls = (
+        'http://www.https://dhesse.github.io/stk_inf_scraping/example.html/',
+    )
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.text, 'lxml')
+        for li in soup('li'):
+            yield {'item': li.text,
+                   'url': response.url}
